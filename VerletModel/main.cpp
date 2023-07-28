c2eed1ac07a2da48797163575386c89e7b1a75a8
Afroz Alam <asteroidalaz@gmail.com>
2023-07-28 08:33:19 +0530

    Initial Commit

---
+++ VerletModel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VerletModel
+//
+//  Created by Afroz Alam on 28/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
